9e9b588cf92f25ec0967feacd77aff963c5be2cb
gprinc <gprinc@itba.edu.ar>
2021-11-02 00:59:08 -0300

    added redux recompose and other libraries

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community'],
+  rules: {
+    complexity: 'off',
+    'import/order': ['error', { 'newlines-between': 'always' }],
+    'no-nested-ternary': 'off',
+    'no-magic-numbers': 'off',
+    'new-cap': 'off'
+  },
+  settings: {
+    'import/ignore': ['node_modules'],
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        settings: {
+          'import/resolver': {
+            node: {
+              paths: ['src'],
+              extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.tsx', '.json']
+            }
+          }
+        }
+      }
+    }
+  }
 };
---
+++ src/redux/store.ts
@@ -0,0 +1,49 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { fetchMiddleware, configureMergeState } from 'redux-recompose';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistReducer } from 'redux-persist';
+import {
+  seamlessImmutableReconciler,
+  seamlessImmutableTransformCreator
+} from 'redux-persist-seamless-immutable';
+import { ImmutableObject } from 'seamless-immutable';
+import { State } from '@interfaces/reduxInterfaces';
+
+
+const transformerConfig = {
+  whitelistPerReducer: {
+  }
+};
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: [],
+  stateReconciler: seamlessImmutableReconciler,
+  transforms: [seamlessImmutableTransformCreator(transformerConfig)]
+};
+
+configureMergeState((state: ImmutableObject<State>, diff: State) => state.merge(diff));
+
+const reducers = combineReducers({
+  
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const middlewares = [];
+const enhancers = [];
+
+/* ------------- Thunk Middleware ------------- */
+middlewares.push(thunk);
+
+/* ------------- Redux-Recompose Middleware ------------- */
+middlewares.push(fetchMiddleware);
+
+/* ------------- Assemble Middleware ------------- */
+enhancers.push(applyMiddleware(...middlewares));
+
+const store = createStore(persistedReducer, compose(...enhancers));
+
+export default store;
