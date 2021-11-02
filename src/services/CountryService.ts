055f1bad733cb0d9ce63d3b16f90461ce16e6844
gprinc <gprinc@itba.edu.ar>
2021-11-02 20:34:47 -0300

    added country service

--- src/services/CountryService.ts
+++ src/services/CountryService.ts
@@ -1,3 +1,15 @@
-export const getCountries = () => {
-  
-};
+import { create } from 'apisauce';
+
+const baseURL = 'https://api.covid19api.com';
+
+const api = create({
+  baseURL,
+  timeout: 5000
+});
+
+const COUNTRIES = '/countries';
+const COUNTRY_DATA =  '/total/dayone/country/{Slug}/status/confirmed';
+
+export const getCountries = () => api.get(COUNTRIES);
+
+export const getCountryDATA = (slug: string) => api.get(COUNTRY_DATA);
