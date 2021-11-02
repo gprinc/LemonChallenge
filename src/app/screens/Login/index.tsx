c882ef0f34449e1779b3f43715bdf309d1ec1824
gprinc <gprinc@itba.edu.ar>
2021-11-02 18:47:52 -0300

    moved app.js

---
+++ src/app/screens/Login/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+import styles from './styles';
+
+const Login = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const handlePress = () => console.warn("clicked");
+
+  return (
+    <SafeAreaView style={[styles.container, backgroundStyle]}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <TouchableOpacity onPress={handlePress}>
+            <Text>Go to Home</Text>
+        </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
