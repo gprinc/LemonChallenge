8d8c55a9fa6a7332e129477f9f6bd8ad77e6d33c
gprinc <gprinc@itba.edu.ar>
2021-11-03 23:15:22 -0300

    configuring library for android

--- src/app/screens/Login/index.tsx
+++ src/app/screens/Login/index.tsx
@@ -1,16 +1,71 @@
 import Routes from '@constants/routes';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   Text,
   TouchableOpacity,
   useColorScheme,
+  View
 } from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 import styles from './styles';
 
 const Login = ({ navigation }: any) => {
+  const [loggedIn, setloggedIn] = useState(false);
+  const [userInfo, setuserInfo] = useState([]);
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const { accessToken, idToken} = await GoogleSignin.signIn();
+      setloggedIn(true);
+      const credential = auth.GoogleAuthProvider.credential(
+        idToken,
+        accessToken,
+      );
+      await auth().signInWithCredential(credential);
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+  const signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      auth().signOut();
+      setloggedIn(false);
+      // setuserInfo([]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['email'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId:
+        '285076797668-k3nkkctd98mrla4fjip7e563psd514bc.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+    });
+  }, []);
+
+
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -24,6 +79,22 @@ const Login = ({ navigation }: any) => {
         <TouchableOpacity onPress={handlePress}>
             <Text>Go to Home</Text>
         </TouchableOpacity>
+        <View style={{ width: '100%' }}>
+            <View style={{ alignItems: 'center' }}>
+              <GoogleSigninButton
+                style={{width: 192, height: 48}}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={signIn}
+              />
+            </View>
+            <View>
+              {!loggedIn && <Text>You are currently logged out</Text>}
+              {loggedIn && (
+                <TouchableOpacity onPress={signOut}>LogOut</TouchableOpacity>
+              )}
+            </View>
+          </View>
     </SafeAreaView>
   );
 };
